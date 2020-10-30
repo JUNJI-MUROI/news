893ff1255396285420d50af66c8bc1bf0ac73fcc
JUNJI-MUROI <67177656+JUNJI-MUROI@users.noreply.github.com>
2020-10-30 19:53:46 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,54 @@
+<!-- footer -->
+    
+<footer>
+
+
+<!-- footer 1 -->
+<div class="row ">
+<div class=" 
+col-lg-4 offset-lg-4 
+col-md-6 offset-md-3
+col-sm-8 offset-sm-2
+pt-4">
+  <img class="img-fluid text-center" src="https://i1.wp.com/tcuprs.com/wp-content/uploads/2018/07/%E3%83%AD%E3%82%B3%E3%82%99%E3%83%9E%E3%83%BC%E3%82%AF%E5%9F%BA%E6%9C%AC%E5%BD%A2-%E9%80%8F%E9%81%8E.png?fit=1024%2C258&ssl=1"/>
+ 
+</div>
+</div>
+
+
+<!-- footer 2 -->
+<div class="row">
+<div class="
+col-lg-4 offset-lg-4 
+col-md-6 offset-md-3
+col-sm-8 offset-sm-2
+mt-2">
+  <ul class="center text-center list-group list-unstyled text-white">
+    <li class="my-1">
+      259-0312
+    </li>
+    <li class="my-1">
+      神奈川県足柄下郡湯河原町吉浜1234-56
+    </li>
+    <li class="my-1">
+      Tel : 03-3454-7966
+    </li>
+    <li class="my-1">
+      Fax: 03-6435-2573
+    </li>
+  </ul>
+</div>
+</div>
+
+
+</footer>
+
+</div>
+
+
+<script src="<?php echo get_template_directory_uri();?>/js.js"></script>
+
+<?php wp_footer();?>
+
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,82 @@
+<!-- 投稿ページ -->
+
+<!-- ヘッダーの読み込み -->
+<?php get_header();?>
+
+<!-- メインエリア -->
+<!-- メインエリア1 -->
+<div class="row">
+
+<!-- メイン部分 -->
+<div class=" col-md-8 col-12">
+
+<div>
+  <!-- 投稿を表示 -->
+<?php if (have_posts()):?>
+<?php while(have_posts()):the_post();?>
+<div>
+ <article id = "post-<?php the_ID();?>" <?php post_class('任意のクラス名を指定');?>>
+   
+  
+    <!-- タイトルを表示 -->
+  
+   <h1><?php the_title();?></h1>
+   
+   <div class="flex">
+     <!-- カテゴリーを表示 -->
+    
+     
+
+   </div>
+  
+
+  <div>
+    <div>
+      <!-- 本文を表示 -->
+      <?php the_content();?>
+    </div>
+  </div>
+
+  
+
+ </article>
+</div>
+<?php endwhile;?>
+<?php endif;?>
+</div>
+
+</div>
+
+ 
+
+
+
+
+
+
+
+
+ <!-- サイドバー -->
+ 
+ <div class="col-4 d-none d-md-block">
+   <div class="mx-3">
+   <h2 class="category-title">
+   お知らせ
+  </h2>
+  <p>弊紙紙面およびWEBで掲載している記事について、読者の皆様にアンケートを行っています。<br>
+アンケートは<a href="https://docs.google.com/forms/d/1iGA2J3X5f6RkoMeLrH0YxfBCYaqdqG84jroQJhZBJHU/viewform?edit_requested=true"><b>こちら</b></a>からご回答ください。<br>
+ご協力のほどよろしくお願いいたします。</p>
+   </div>
+ </div>
+
+</div>
+
+
+
+
+
+
+
+
+<!-- フッターの読み込み -->
+<?php get_footer();?>
